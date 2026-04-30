e3cd5654c7fd0a869a32f5750e2b0b63acb0dfe1
VictorRMarcelino <httpsvictorramos@gmail.com>
2026-04-29 21:21:21 -0300

    feat: User Factory/Seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make(env('CAMPAING_TRACKER_USER_PASSWORD', 'default_admin_password')),
             'remember_token' => Str::random(10),
         ];
     }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,24 @@
 @extends('layout.index')
 
 @section('content')
-
+<div>
+    <form action="" method="POST">
+        @csrf
+        <div>
+            <label for="">User:</label>
+            <input type="text" name="user" id="user">
+        </div>
+        <div>
+            <label for="">Email:</label>
+            <input type="email" name="email" id="email">
+        </div>
+        <div>
+            <label for="">Password:</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <div>
+            <input type="submit">
+        </div>
+    </form>
+</div>
 @endsection
