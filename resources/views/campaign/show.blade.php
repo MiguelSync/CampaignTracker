c64282ce9a765c99c8d7cbff97e880ede07b147b
VictorRMarcelino <httpsvictorramos@gmail.com>
2026-05-13 21:40:01 -0300

    Removed Commented Line

--- resources/views/campaign/show.blade.php
+++ resources/views/campaign/show.blade.php
@@ -17,7 +17,6 @@
         <div class="w-full mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div>
-                    {{-- <a href="{{ route('') }}"><x-primary-button>{{ __("Adicionar Jogador") }}</x-primary-button></a> --}}
                 </div>
                 <div class="w-full">
                     <livewire:campaign-user.campaign-user-list 
