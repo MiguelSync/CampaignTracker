6068229a4e8025d5d65cee87e76db0f6f9902c9c
VictorRMarcelino <httpsvictorramos@gmail.com>
2026-05-17 22:00:34 -0300

    feature: Add validation to let add the same user in a campaign

--- app/Http/Controllers/CampaignUser/CampaignUserController.php
+++ app/Http/Controllers/CampaignUser/CampaignUserController.php
@@ -8,19 +8,28 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CampaignUser\CampaignUserStoreRequest;
 use App\Models\Campaign;
 use App\Models\CampaignUser;
+use Exception;
+use Throwable;
 
 class CampaignUserController extends Controller
 {
 
     public function store(CampaignUserStoreRequest $request, Campaign $campaign) {
-        $input = $request->validated();
-        CampaignUserStoreAction::run($input, $campaign);
-        return redirect()->route('campaign.show', [
-            'campaign' => $campaign
-        ]);
+        try {
+            $input = $request->validated();
+            CampaignUserStoreAction::run($input, $campaign);
+            return redirect()->route('campaign.show', [
+                'campaign' => $campaign
+            ]);
+        } catch (Throwable $ex) {
+            return $this->handleExceptionBackWithErrors($ex);
+        } catch (Exception $ex) {
+            return $this->handleExceptionBackWithErrors($ex);
+        }
     }
 
     public function destroy(CampaignUser $campaignUser) {
         CampaignUserDestroyAction::run($campaignUser);
+        return redirect()->back();
     }
 }
